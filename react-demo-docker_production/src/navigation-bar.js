65dc38c33a1ae7ca664c9ca1ea335883d466b9cb
Dan Cristian Deac <dandeac26@gmail.com>
2024-01-15 13:12:54 +0200

    chat basic finish

--- react-demo-docker_production/src/navigation-bar.js
+++ react-demo-docker_production/src/navigation-bar.js
@@ -21,6 +21,7 @@ import { TabContent, TabPane, NavItem } from "reactstrap";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 import logo from "./commons/images/icon.png";
+import { v4 as uuidv4 } from "uuid";
 
 const textStyle = {
   color: "white",
@@ -58,79 +59,95 @@ function NavigationBar() {
   // }, [messages]);
   useEffect(() => {
     connect();
+    scrollToBottom();
     return () => {
       disconnect();
     };
   }, []);
 
-  const handleIncomingMessage = (message) => {
-    const receivedMessage = JSON.parse(message.body);
-    // Determine whether the message is incoming or outgoing
-    const messageSender =
-      receivedMessage.sender === userData.username
-        ? receivedMessage.destination
-        : receivedMessage.sender;
+  // const handleIncomingMessage = (message) => {
+  //   const receivedMessage = JSON.parse(message.body);
+  //   // Determine whether the message is incoming or outgoing
+  //   const messageSender =
+  //     receivedMessage.sender === userData.username
+  //       ? receivedMessage.destination
+  //       : receivedMessage.sender;
 
-    setActiveChats((prevChats) => {
-      const updatedChats = { ...prevChats };
-      const chatMessages = updatedChats[messageSender] || { messages: [] };
-      chatMessages.messages.push(receivedMessage);
+  //   setActiveChats((prevChats) => {
+  //     const updatedChats = { ...prevChats };
+  //     const chatMessages = updatedChats[messageSender] || { messages: [] };
+  //     chatMessages.messages.push(receivedMessage);
 
-      updatedChats[messageSender] = chatMessages;
-      return updatedChats;
-    });
-  };
+  //     updatedChats[messageSender] = chatMessages;
+  //     return updatedChats;
+  //   });
+  // };
 
   const toggleModal = () => setModal(!modal);
 
+  // const connect = () => {
+  //   // Mock connection to WebSocket
+  //   const socket = new SockJS("http://localhost:8088/websocket-endpoint");
+  //   const client = Stomp.over(socket);
+
+  //   client.connect({}, () => {
+  //     client.subscribe("/topic/messages", (message) => {
+  //       const receivedMessage = JSON.parse(message.body);
+  //       setActiveChats((prevChats) => {
+  //         const updatedChats = { ...prevChats };
+  //         const chatKey =
+  //           receivedMessage.sender === userData.username
+  //             ? receivedMessage.destination
+  //             : receivedMessage.sender;
+  //         if (!updatedChats[chatKey]) {
+  //           updatedChats[chatKey] = { messages: [] };
+  //         }
+  //         console.log("NEW MESSAGE ADDED: " + receivedMessage);
+  //         updatedChats[chatKey].messages.push(receivedMessage);
+  //         return updatedChats;
+  //       });
+  //     });
+  //   });
+
+  //   setStompClient(client);
+  // };
   const connect = () => {
     // Mock connection to WebSocket
     const socket = new SockJS("http://localhost:8088/websocket-endpoint");
     const client = Stomp.over(socket);
 
-    // client.connect({}, () => {
-
-    //   client.subscribe("/topic/messages", (message) => {
-    //     const receivedMessage = JSON.parse(message.body);
-
-    //     // If the message is from the admin and this client is the admin, ignore it
-    //     if (receivedMessage.sender === userData.username && isAdmin) {
-    //       return;
-    //     }
-
-    //     setActiveChats((prevChats) => {
-    //       const updatedChats = { ...prevChats };
-    //       const chatKey = isAdmin ? receivedMessage.sender : "admin";
-
-    //       const chatMessages = updatedChats[chatKey] || { messages: [] };
-    //       chatMessages.messages.push(receivedMessage);
-
-    //       updatedChats[chatKey] = chatMessages;
-    //       return updatedChats;
-    //     });
-
-    //     // If there is no selected chat, set it to the sender of the first received message
-    //     if (!selectedChat && receivedMessage.sender !== userData.username) {
-    //       setSelectedChat(receivedMessage.sender);
-    //     }
-    //   });
-    // });
-
     client.connect({}, () => {
       client.subscribe("/topic/messages", (message) => {
         const receivedMessage = JSON.parse(message.body);
-        setActiveChats((prevChats) => {
-          const updatedChats = { ...prevChats };
-          const chatKey =
-            receivedMessage.sender === userData.username
-              ? receivedMessage.destination
-              : receivedMessage.sender;
-          if (!updatedChats[chatKey]) {
-            updatedChats[chatKey] = { messages: [] };
-          }
-          updatedChats[chatKey].messages.push(receivedMessage);
-          return updatedChats;
-        });
+
+        if (
+          receivedMessage.destination === userData.username ||
+          receivedMessage.sender === userData.username
+        ) {
+          setActiveChats((prevChats) => {
+            const updatedChats = { ...prevChats };
+            const chatKey =
+              receivedMessage.sender === userData.username
+                ? receivedMessage.destination
+                : receivedMessage.sender;
+
+            if (!updatedChats[chatKey]) {
+              updatedChats[chatKey] = { messages: [] };
+            }
+
+            // Check if the message already exists based on a unique identifier
+            const messageExists = updatedChats[chatKey].messages.some(
+              (msg) => msg.id === receivedMessage.id // Assuming each message has a unique 'id'
+            );
+
+            if (!messageExists) {
+              updatedChats[chatKey].messages.push(receivedMessage);
+              console.log("NEW MESSAGE ADDED: ", receivedMessage);
+            }
+
+            return updatedChats;
+          });
+        }
       });
     });
 
@@ -146,7 +163,15 @@ function NavigationBar() {
 
   const handleSendMessage = () => {
     if (currentMessage.trim() !== "" && stompClient) {
+      // const messageId =
+      //   Date.now().toString() +
+      //   "-" +
+      //   Math.random()
+      //     .toString(36)
+      //     .substr(2, 9);
+
       const newMessage = {
+        id: uuidv4(),
         sender: userData.username,
         destination: isAdmin ? selectedChat : "admin", // Admin sends to selected user, client sends to admin
         text: currentMessage,
@@ -157,72 +182,6 @@ function NavigationBar() {
       setCurrentMessage("");
     }
   };
-  // const handleSendMessage = () => {
-  //   let userData = JSON.parse(localStorage.getItem("authenticatedUser"));
-  //   // let destination = userData.roles[0] === "ADMIN" ? "user" : "admin";
-  //   let isAdmin =
-  //     userData &&
-  //     Array.isArray(userData.roles) &&
-  //     userData.roles[0] === "ADMIN";
-  //   let destination = isAdmin ? selectedChat : "admin";
-
-  //   console.log("Message destination:", destination);
-
-  //   if (currentMessage.trim() && stompClient) {
-  //     const newMessage = {
-  //       sender: userData.username,
-  //       destination: destination,
-  //       text: currentMessage,
-  //       seen: false,
-  //       role: userData.roles[0],
-  //     };
-  //     // if (!isAdmin) {
-  //     //   setActiveChats((prevChats) => {
-  //     //     const updatedChats = { ...prevChats };
-  //     //     // Assuming 'destination' holds the username of the client
-  //     //     if (!updatedChats[destination]) {
-  //     //       updatedChats[destination] = { messages: [] };
-  //     //     }
-  //     //     updatedChats[destination].messages.push(newMessage);
-  //     //     return updatedChats;
-  //     //   });
-  //     // }
-  //     // if (isAdmin) {
-  //     //   setActiveChats((prevChats) => {
-  //     //     const updatedChats = { ...prevChats };
-  //     //     // Assuming 'destination' holds the username of the client
-  //     //     if (!updatedChats[destination]) {
-  //     //       updatedChats[destination] = { messages: [] };
-  //     //     }
-  //     //     updatedChats[destination].messages.push(newMessage);
-  //     //     return updatedChats;
-  //     //   });
-  //     // }
-
-  //     if (!isAdmin) {
-  //       setActiveChats((prevChats) => {
-  //         const updatedChats = { ...prevChats };
-  //         const userChat = updatedChats["admin"] || { messages: [] };
-  //         userChat.messages.push(newMessage);
-  //         updatedChats["admin"] = userChat;
-  //         return updatedChats;
-  //       });
-  //     }
-  //     stompClient.send("/app/send", {}, JSON.stringify(newMessage));
-  //     setCurrentMessage("");
-  //   }
-  // };
-
-  // const handleIncomingMessage = (msg) => {
-  //   setActiveChats((prevChats) => {
-  //     const chat = prevChats[msg.sender] || { messages: [] };
-  //     chat.messages.push(msg);
-  //     return { ...prevChats, [msg.sender]: chat };
-  //   });
-  //   if (!activeTab) {
-  //     setActiveTab(msg.sender);
-  //   }
-  // };
 
   const modalStyle = {
     maxWidth: "500px",
@@ -262,65 +221,6 @@ function NavigationBar() {
           <Button style={{ backgroundColor: "Green" }} href="/login">
             Login
           </Button>
-          {/* <Modal
-            isOpen={modal}
-            toggle={toggleModal}
-            onOpened={connect}
-            onClosed={disconnect}
-            style={modalStyle}
-          >
-            <ModalHeader toggle={toggleModal}>Chat</ModalHeader>
-            <ModalBody>
-              <div
-                style={{
-                  maxHeight: "300px",
-                  overflowY: "auto",
-                  padding: "10px",
-                  height: "600px",
-                }}
-              >
-                {messages.map((msg, index) => (
-                  <div
-                    key={index}
-                    style={{
-                      textAlign: msg.sender === "user" ? "right" : "left",
-                      margin: "5px 0",
-                    }}
-                  >
-                    <span
-                      style={{
-                        backgroundColor:
-                          msg.sender === "user" ? "#e0e0e0" : "#f0f0f0",
-                        borderRadius: "10px",
-                        padding: "10px 15px",
-                        display: "inline-block",
-                        maxWidth: "80%",
-                      }}
-                    >
-                      {msg.text}
-                    </span>
-                  </div>
-                ))}
-                <div ref={messagesEndRef} />
-              </div>
-              <FormGroup>
-                <Input
-                  type="textarea"
-                  placeholder="Type your message here..."
-                  value={currentMessage}
-                  onChange={(e) => setCurrentMessage(e.target.value)}
-                />
-              </FormGroup>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onClick={handleSendMessage}>
-                Send
-              </Button>{" "}
-              <Button color="secondary" onClick={toggleModal}>
-                Close
-              </Button>
-            </ModalFooter>
-          </Modal> */}
           <Modal
             isOpen={modal}
             toggle={toggleModal}
@@ -330,25 +230,25 @@ function NavigationBar() {
           >
             <ModalHeader toggle={toggleModal}>Chat</ModalHeader>
             <ModalBody key={selectedChat}>
-              {isAdmin && (
-                <Nav tabs>
-                  {Object.keys(activeChats).map((user) => (
-                    <NavItem key={user}>
-                      <NavLink
-                        className={classnames({
-                          active: selectedChat === user,
-                        })}
-                        onClick={() => {
-                          console.log("Setting selected chat to user:", user);
-                          setSelectedChat(user);
-                        }}
-                      >
-                        Chat with {user}
-                      </NavLink>
-                    </NavItem>
-                  ))}
-                </Nav>
-              )}
+              {/*{isAdmin && (*/}
+              <Nav tabs>
+                {Object.keys(activeChats).map((user) => (
+                  <NavItem key={user}>
+                    <NavLink
+                      className={classnames({
+                        active: selectedChat === user,
+                      })}
+                      onClick={() => {
+                        console.log("Setting selected chat to user:", user);
+                        setSelectedChat(user);
+                      }}
+                    >
+                      Chat with {user}
+                    </NavLink>
+                  </NavItem>
+                ))}
+              </Nav>
+              {/* )}  */}
               <TabContent activeTab={selectedChat}>
                 {Object.keys(activeChats).map((user) => (
                   <TabPane tabId={user} key={user}>
